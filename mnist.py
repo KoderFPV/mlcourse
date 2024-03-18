184904d50be97b783c2b8e85dc342d2de03d91c2
Artur Słomowski <artur.slomowski@gmail.com>
2024-03-18 18:22:07 +0100

    revert

--- mnist.py
+++ mnist.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras.datasets import mnist
-from keras.optimizers import Adam
-
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 train_images = train_images/255.0
 test_images = test_images/255.0
@@ -11,19 +9,18 @@ model = keras.Sequential([
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-
+from keras import optimizers
+from keras.optimizers import Adam
 optimizer = Adam()
 model.compile(optimizer=optimizer,
               loss='sparse_categorical_crossentropy')
 model.fit(train_images, train_labels, epochs=5)
-evaluation_result = model.evaluate(test_images, test_labels)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
 if isinstance(evaluation_result, float):
     test_loss = test_acc = evaluation_result
 else:
     test_loss, test_acc = evaluation_result
 print(f'Testaccuracy: {test_acc}')
-
 classifications = model.predict(test_images)
-
 print(classifications)
-print(test_labels)
+print(test_labels(0))
